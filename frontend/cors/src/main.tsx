70c17fdcbe70b6946dbdec71a3ecc8d666a5cf3f
Patrick Arminio <patrick.arminio@gmail.com>
2023-08-14 13:38:45 +0100

    Fix cors

--- frontend/cors/src/main.tsx
+++ frontend/cors/src/main.tsx
@@ -23,7 +23,7 @@ const errorLink = onError(({ graphQLErrors }) => {
 const link = from([
   errorLink,
   new HttpLink({
-    uri: "http://localhost:4000",
+    uri: "http://localhost:4000/graphql",
     credentials: "include",
   }),
 ]);
